21efad72723547ed0584c3e91edc371e2bfcc4e9
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-07-24 20:20:12 -0300

    feat(auth): add role-based access control with guard and decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '../auth/jwt.guard';
 import { AuthRequestDto } from 'src/auth/dto/auth-request.dto';
+import { RolesGuard } from 'src/commom/guards/roles.guard';
+import { Roles } from 'src/commom/decorators/roles.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -26,14 +28,16 @@ export class UsersController {
   }
 
   // Rotas autenticadas
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Get()
   findAll(@Request() req: AuthRequestDto) {
     if (req.user.role !== 'ADMIN') return [{ id: req.user.userId }];
     return this.usersService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'USER')
   @Get(':id')
   findOne(@Param('id') id: string, @Request() req: AuthRequestDto) {
     if (req.user.role !== 'ADMIN' && req.user.userId !== id)
@@ -41,7 +45,8 @@ export class UsersController {
     return this.usersService.findOne(id);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'USER')
   @Put(':id')
   update(
     @Param('id') id: string,
@@ -52,7 +57,8 @@ export class UsersController {
     return this.usersService.update(id, dto, user);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'USER')
   @Delete(':id')
   remove(@Param('id') id: string, @Request() req: AuthRequestDto) {
     const user = { id: req.user.userId, role: req.user.role };
