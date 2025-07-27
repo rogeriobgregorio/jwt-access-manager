5353b23e786941adaab444579df191cb66cdd35b
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-07-27 17:21:40 -0300

    refactor(core): improve application bootstrap logic

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 import { HttpExceptionFilter } from './commom/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 void bootstrap();
